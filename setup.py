5af3defbe59ab7cc32409745193963a0d8be5cad
fedecech <federicocech@gmail.com>
2021-10-01 06:18:03 +0100

    added setup package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='form_automator',
+    version='0.1.0',
+    packages=['form_automator'],
+    install_requires=[],
+    python_requires=">=3.8",
+    description="Automator package"
+)
