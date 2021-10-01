8a76e8b650d706b3739a8440a181d7f7f8c06189
fedecech <federicocech@gmail.com>
2021-10-01 06:22:32 +0100

    init package

---
+++ form_automator/__init__.py
@@ -0,0 +1,7 @@
+from .Task import Task
+from .Calendar import Calendar
+from .Event import Event
+from .Source import Source
+from .Provider import Provider
+from .Util import Util
+from .MicrosoftSource import MicrosoftSource
