e69f37339971096a5ad780fd81593c1ce0174ad5
fedecech <federicocech@gmail.com>
2021-10-02 11:24:42 +0100

    form subpackage init

---
+++ form_automator/form/__init__.py
@@ -0,0 +1,7 @@
+from .DatePicker import DatePicker
+from .Form import Form
+from .FormComponent import FormComponent
+from .FormComponentType import FormComponentType
+from .Radio import Radio
+from .Select import Select
+from .Text import Text
