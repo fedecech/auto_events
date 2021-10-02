676e4921c9a83b7b3a553c6cbe733472dca0a7d7
fedecech <federicocech@gmail.com>
2021-10-02 11:32:50 +0100

    imported code from test file and implemented new library api

--- main.py
+++ main.py
@@ -1,38 +1,75 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
+from time import sleep
 import os
-load_dotenv()
+from dotenv.main import load_dotenv
 
+from form_automator.Calendar import Calendar as AutoCalendar
+from form_automator.Event import Event as AutoEvent
+from form_automator.MicrosoftSource import MicrosoftSource
+from form_automator.Task import Task as AutoTask
+from form_automator.form.Form import Form as AutoForm
+
+
+load_dotenv('/Users/fedecech/form_automator/.env')
+
+client_id = os.getenv('CLIENT_ID')
+secret = os.getenv('API_SECRET')
 email = os.getenv('EMAIL')
 psw = os.getenv('PASSWORD')
-driver = webdriver.Chrome('/Users/fedecech/Selenium Drivers/chromedriver')
+SLEEP_TIME = 10
 
-driver.get('https://forms.office.com/r/mVZKDrD0Rg')
+credentials = (client_id, secret)
 
-# Wait page is loaded
-time.sleep(5)
 
-current_url: str = driver.current_url
+def to_run(source: 'MicrosoftSource'):
+    form = AutoForm(url='https://forms.office.com/r/mVZKDrD0Rg',
+                    email_confirmation=True, source=source)
+    form.fill_in(responses=["Option 1", "22/02/2021",
+                 "Option 4", "Hey there, it's working"])
+    print("[MicrosoftSource] 1 . Task runned from inside map func")
+    return True
 
-print(current_url)
-# user is not logged in
-if ("login" in current_url):
-    print("Logging in using credentials")
-    email_input = driver.find_element_by_id("i0116")
 
-    email_input.clear()
-    email_input.send_keys(email)
-    email_input.send_keys(Keys.RETURN)
+def add_task_to_test_events(source: 'MicrosoftSource', event: 'AutoEvent') -> 'AutoEvent':
+    task = AutoTask(run_date=event.start_date, to_run=lambda: to_run(source=source), on_success=lambda: print(
+        "[MicrosoftSource] 2 . success callback"))
+    task1 = AutoTask(run_date=event.end_date, to_run=lambda: to_run(source=source), on_success=lambda: print(
+        "[MicrosoftSource] 2 . success callback"))
+    event.add_task(task)
+    event.add_task(task1)
+    return event
 
-    time.sleep(2)
 
-    password_input = driver.find_element_by_id("i0118")
+def filter(e: 'AutoEvent'):
+    LOCAL_TIMEZONE = datetime.now(timezone.utc).astimezone().tzinfo
+    localised_date = datetime.now().replace(tzinfo=LOCAL_TIMEZONE)
+    return "Test Automation" in e.title and e.end_date + timedelta(seconds=SLEEP_TIME/2) > localised_date
 
-    password_input.clear()
-    password_input.send_keys(psw)
-    password_input.send_keys(Keys.RETURN)
-time.sleep(10)
 
-driver.close()
+def main():
+    source = MicrosoftSource(credentials, account_cred={
+                             'email': email, 'password': psw})
+
+    calendar = AutoCalendar(
+        source=source, filter=filter, map=add_task_to_test_events)
+
+    # date = datetime.now()
+    # date = date + timedelta(seconds=2)
+
+    # task = AutoTask(to_run=lambda: to_run(source))
+    # event1 = AutoEvent(trigger_date=date, start_date=date,
+    #                    end_date=date, title="Event 1", tasks=[task])
+
+    # calendar = AutoCalendar(from_source=False, source=source, events=[event1])
+    calendar.scheduler.start()
+
+    while True:
+        print('Fetching new data...')
+        calendar.update()
+        sleep(SLEEP_TIME)  # update very 10 secs
+
+    source.driver.close()
+
+
+if __name__ == "__main__":
+    main()
