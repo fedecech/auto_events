fe726d5d48a8a4902dee4b7fdf608f2e7d794a96
fedecech <federicocech@gmail.com>
2021-10-08 05:37:35 +0100

    modify setup

--- auto_events/__init__.py
+++ auto_events/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.0.1'
+
 from .Task import Task
 from .Calendar import Calendar
 from .Event import Event
--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup
+from auto_events.__init__ import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='form_automator',
-    version='0.1.0',
-    packages=['form_automator'],
+    name='auto_events',
+    version=__version__,
+    url="https://github.com/fedecech/auto_events",
+    author="Federico Cecchinato",
+    author_email="federicocech@gmail.com",  # use icloud fake email
+    description="Automate tasks easily using python",
+    long_description=long_description,
+    license='MIT',
+    packages=['auto_events'],
+    py_modules=['auto_events'],
     install_requires=[],
     python_requires=">=3.8",
-    description="Automator package"
 )
